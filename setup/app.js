ec07fe203d151caafbc9d3a7678d8573ab2a008f
Summer Duke <esrduke@gmail.com>
2020-08-04 20:47:17 -0500

    add/subtract buttons

--- setup/app.js
+++ setup/app.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaay');
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+let display = 0;
+
+const addValue = () => {
+    let domString = '';
+    domString += display++;
+    printToDom('displayValue', domString);
+}
+
+const subtractValue = () => {
+    let domString = '';
+    domString += display--;
+    printToDom('displayValue', domString);
+}
+
+const onClickEvents = () => {
+    document.querySelector("#addButton").addEventListener("click", addValue);
+    document.querySelector("#minusButton").addEventListener("click", subtractValue);
+};
+
+const init = () => {
+    onClickEvents();
+}
+
+init();
