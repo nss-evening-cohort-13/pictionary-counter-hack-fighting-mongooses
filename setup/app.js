c227607a4c1b8f29cacb921f95da2137c8f337a3
Liz <liz@Lizs-Mac.local>
2020-08-04 20:25:50 -0500

    created js function to change colors of the displayed value to represent positive, negative or neutral value

--- setup/app.js
+++ setup/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaay');
+
+
+
+const colorValue= () => {
+    if (displayValue > 0) {
+    document.querySelector('displayValue').style.color = '#25B14C'
+    } else if (displayValue < 0) {
+    document.querySelector('displayValue').style.color = '#D23A3A'
+    } else {
+    document.querySelector('displayValue').style.color = '#000000'
+    }
+};
